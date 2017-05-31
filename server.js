cbabc79f246d893c55177254f68d2d3aa0cd98ff
dhysong <dhysong@gmail.com>
2017-05-31 13:53:16 -0400

    Trying to publish to azure

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 var http = require('http');
 
+var express = require('express'), 
+    sio = require('socket.io')
+    phoneStatus = require('./phone-utils.js');
+
 http.createServer(function (req, res) {
     
     res.writeHead(200, { 'Content-Type': 'text/html' });
