3dde43a753ee46c184779060606a66649f60c45f
dhysong <dhysong@gmail.com>
2017-05-31 13:55:18 -0400

    Trying to publish to azure

--- server.js
+++ server.js
@@ -9,6 +9,13 @@ var app = express.createServer();
 app.use(express.bodyParser());
 app.use(express.static(__dirname + '/public'));
 
+var io = sio.listen(app);
+
+io.on('connection', function(socket) {
+    io.emit('phone-status', { 'phone-status': phoneStatus });
+    console.log('client connected');
+});
+
 http.createServer(function (req, res) {
     
     res.writeHead(200, { 'Content-Type': 'text/html' });
