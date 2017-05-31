78be5daf6298b9266a000f7fd445cd6db47fdb78
dhysong <dhysong@gmail.com>
2017-05-31 13:54:35 -0400

    Trying to publish to azure

--- server.js
+++ server.js
@@ -4,6 +4,11 @@ var express = require('express'),
     sio = require('socket.io')
     phoneStatus = require('./phone-utils.js');
 
+
+var app = express.createServer();
+app.use(express.bodyParser());
+app.use(express.static(__dirname + '/public'));
+
 http.createServer(function (req, res) {
     
     res.writeHead(200, { 'Content-Type': 'text/html' });
