0d416b4e11cfa7d3cb2bad15577d46b8c80fbc2d
dhysong <dhysong@gmail.com>
2017-05-31 11:18:20 -0400

    Trying to publish to azure

--- server.js
+++ server.js
@@ -1,8 +1,30 @@
-var http = require('http');
+var express = require('express'), 
+    sio = require('socket.io')
+    phoneStatus = require('./phone-utils.js');
 
-http.createServer(function (req, res) {
-    
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.end('Hello, world!');
-    
-}).listen(process.env.PORT || 8080);
+
+var app = express.createServer();
+app.use(express.bodyParser());
+app.use(express.static(__dirname + '/public'));
+
+var io = sio.listen(app);
+
+io.on('connection', function(socket) {
+    io.emit('phone-status', { 'phone-status': phoneStatus });
+    console.log('client connected');
+});
+
+app.post('/', function (req, res) {
+  messages = req.param('messages', { 'Messages': [] });
+  
+  phoneStatus.Messages = messages;
+  
+  io.emit('phone-status', { 'phone-status': phoneStatus });
+  
+  res.send('{Result: "successful"');
+});
+
+app.listen(80, function () {
+  var addr = app.address();
+  console.log('   app listening on http://' + addr.address + ':' + addr.port);
+});
